7d75eb177d1f13de495d095114fbd4e86c22c143
leewonz <lwk530@gmail.com>
2018-11-25 20:27:36 +0900

    init (new)

---
+++ vutest/Assets/Scripts/TargetEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetEvent : MonoBehaviour,ITrackableEventHandler  {
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    public bool isDetected = false;
+    public Transform targetTransform;
+
+    void Start()
+    {
+        targetTransform = transform;
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            isDetected = true;
+            print("Detected");
+        }
+        else
+        {
+            isDetected = false;
+            print("UnDetected");
+        }
+    }
+}
---
+++ vutest/Assets/Scripts/TargetManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour {
+
+    public TargetEvent[] targetEvents;
+    public enum TargetNames {forestBase, tree, animal };
+    public GameObject testCube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < targetEvents.Length; i++)
+        {
+            if(targetEvents[i].isDetected)
+            {
+                print(i + "detected");
+            }
+        }
+
+        if (targetEvents[(int)TargetNames.forestBase].isDetected)
+        {
+            if (targetEvents[(int)TargetNames.tree].isDetected)
+            {
+                Instantiate(testCube, targetEvents[(int)TargetNames.forestBase].targetTransform);
+            }
+            if (targetEvents[(int)TargetNames.animal].isDetected)
+            {
+                Instantiate(testCube, targetEvents[(int)TargetNames.forestBase].targetTransform);
+            }
+        }
+
+    }
+}
