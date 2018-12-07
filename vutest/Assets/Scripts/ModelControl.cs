fe09df531e721f594c745884895a050e5d62ab24
leewonz <lwk530@gmail.com>
2018-12-07 13:19:18 +0900

    Update

--- vutest/Assets/Scripts/ModelControl.cs
+++ vutest/Assets/Scripts/ModelControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ModelControl : MonoBehaviour {
 
+    public bool DeleteOnLost;
     //
 
 	// Use this for initialization
@@ -20,5 +21,33 @@ public class ModelControl : MonoBehaviour {
             Mathf.Clamp(transform.rotation.eulerAngles.z, 50, 80)));
             */
 	}
-    
+
+
+    public void ModelDetected() 
+    {
+        foreach (ModelControl childControl in transform.GetComponentsInChildren<ModelControl>())
+        {
+            childControl.ChildModelDetected();
+        }
+    }
+
+    public void ModelLost()
+    {
+        foreach (ModelControl childControl in transform.GetComponentsInChildren<ModelControl>())
+        {
+            childControl.ChildModelLost();
+        }
+    }
+
+    public void ChildModelDetected() //감지될 때 실행되는 모든 자식 모델의 코드
+    {
+        print(gameObject + " child detected");
+    }
+
+    public void ChildModelLost()  //사라질 때 실행되는 모든 자식 모델의 코드
+    {
+        print(gameObject + " child lost");
+    }
+
+
 }
--- vutest/Assets/Scripts/TargetEvent.cs
+++ vutest/Assets/Scripts/TargetEvent.cs
@@ -9,16 +9,23 @@ public class TargetEvent : MonoBehaviour,ITrackableEventHandler  {
 
     public bool isDetected = false;
     public Transform targetTransform; //오브젝트가 올라갈 곳
-    public GameObject modelObject; //오브젝트
+    public GameObject modelObject; //오브젝트 인스턴스 (끄고 켜짐)
+    public GameObject modelObjectCreated; //생성될 오브젝트
+    GameObject modelInstCreated; //생성된 오브젝트 인스턴스
+
 
     void Start()
     {
+        //오브젝트가 올라갈 트랜스폼 설정
         if (targetTransform == null)
         {
             targetTransform = transform;
         }
-        modelObject.transform.parent = targetTransform;
-
+        if (modelObject != null)
+        {
+            modelObject.transform.parent = targetTransform;
+        }
+        //뷰포리아 초기화
         mTrackableBehaviour = GetComponent<TrackableBehaviour>();
         if (mTrackableBehaviour)
         {
@@ -46,14 +53,41 @@ public class TargetEvent : MonoBehaviour,ITrackableEventHandler  {
     void Detected()
     {
         print("Detected");
-        modelObject.GetComponent<Renderer>().enabled = true;
+        
+        //모델의 감지 코드 호출
+        if (modelObject != null)
+        {
+            ModelControl modelInstControl = modelObject.GetComponent<ModelControl>();
+            modelInstControl.ModelDetected();
+        }
 
-
+        //모델 생성, 모델의 감지 코드 호출
+        if (modelObjectCreated != null)
+        {
+            modelInstCreated = Instantiate(modelObjectCreated, targetTransform);
+            ModelControl modelInstCreatedControl = modelInstCreated.GetComponent<ModelControl>();
+            modelInstCreatedControl.ModelDetected();
+        }
     }
     void Lost()
     {
         print("UnDetected");
+        
+        //모델 사라짐 코드 호출
+        if (modelObject != null)
+        {
+            
+            ModelControl modelInstControl = modelObject.GetComponent<ModelControl>();
+            modelInstControl.ModelLost();
+        }
 
+        //모델(생성된) 사라짐 코드 호출
+        if (modelObjectCreated != null)
+        {
+            
+            ModelControl modelInstCreatedControl = modelInstCreated.GetComponent<ModelControl>();
+            modelInstCreatedControl.ModelLost();
+        }
     }
 
 }
